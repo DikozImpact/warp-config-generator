238847a1d3969d2cbb6e25b5d31cb720ae9ca8cb
DikozImpact <22dikoz22@gmail.com>
2025-11-12 16:15:15 +0300

    Update Karing.js

--- Karing.js
+++ Karing.js
@@ -79,7 +79,7 @@ async function generateWarpConfig() {
 "type": "wireguard",
 "local_address": ["${client_ipv4}/32", "${client_ipv6}/128"],
 "peer_public_key": "${peer_pub}",
-"server": "188.114.97.1",
+"server": "8.6.112.1",
 "server_port": 500
 }
   ]
